11e80cd40a3888160e49b6d1040e54ac64d2807d
John Soto <johnsoto90@gmail.com>
2021-10-18 03:29:39 -0300

    refactor

--- main.js
+++ main.js
@@ -1,26 +1,30 @@
-import person_1 from './assets/images/person_1.jpg'
-import person_2 from './assets/images/person_2.jpg'
-import person_3 from './assets/images/person_3.jpg'
-import person_4 from './assets/images/person_4.jpg'
 
 const select = (element) => {
   return document.querySelector(element)
 }
 
 const postsUrl = "https://jsonplaceholder.typicode.com/posts"
-const usersUrl = "https://jsonplaceholder.typicode.com/users"
+const usersUrl = "https://randomuser.me/api/0.8/?results=4"
 
 let testimonials = {}
 let currentTestimonial;
 const transitionTimer = 10;
 let transition;
 
-const imgUrl = (n) => {
-  if(n === 1) return person_1
-  if(n === 2) return person_2
-  if(n === 3) return person_3
-  else return person_4
+function toCamelCase(word) {
+  return word.split('').reduce((previus, current, index) => {
+    if(index == 0) return previus += current.toUpperCase()
+    return previus += current;
+  }, '')
 }
+function processUserData(data)
+{
+  return [...data.results].map((item) => ({
+    name: `${toCamelCase(item.user.name.title)} ${toCamelCase(item.user.name.first)} ${toCamelCase(item.user.name.last)}`,
+    img: item.user.picture.large,
+  }));
+}
+
 async function getData(url) {
   let data = await fetch(url)
   .then(res => res.json())
@@ -31,7 +35,9 @@ async function getData(url) {
 async function init(){
   let users = await getData(usersUrl)
   let posts = await getData(postsUrl)
-
+  console.log(users)
+  users = processUserData(users)
+  console.log(users)
   if(Object.keys(posts).length === 0 || Object.keys(users).length === 0) {
     console.error('ERROR FETCHING DATA')
     return;
@@ -62,7 +68,7 @@ const setTestimonial = (n) => {
   button.classList.add('bg-green')
   
   let preloadImage = new Image()
-  preloadImage.src = imgUrl(n)
+  preloadImage.src = testimonials.users[n - 1].img
   
   if(currentTestimonial != undefined){
     let prev = select(`#control-${currentTestimonial}`)
@@ -71,17 +77,17 @@ const setTestimonial = (n) => {
     container.classList.remove('fade-in')
     container.classList.add('fade-out')
     setTimeout(() => {
-      avatar.src = imgUrl(n)
-      name.innerHTML = testimonials.users[n].name
-      text.innerHTML = testimonials.posts[n].body
+      avatar.src = testimonials.users[n - 1].img
+      name.innerHTML = testimonials.users[n - 1].name
+      text.innerHTML = testimonials.posts[n - 1].body
       container.classList.remove('fade-out')
       container.classList.add('fade-in')
     }, 980)
   }
   else{
-    avatar.src = imgUrl(n)
-    name.innerHTML = testimonials.users[n].name
-    text.innerHTML = testimonials.posts[n].body
+    avatar.src = testimonials.users[n - 1].img
+    name.innerHTML = testimonials.users[n - 1].name
+    text.innerHTML = testimonials.posts[n - 1].body
   }
   currentTestimonial = n;
   let next = n >= 4 ? 1 : n + 1 
